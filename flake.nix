261f010efe52ae413a1aa44f879e1de283187d57
Ji-Haeng Huh <jhhuh.note@gmail.com>
2023-07-05 00:40:47 +0900

    use asgard-labs/hasktorch-nix for cuda support

--- flake.nix
+++ flake.nix
@@ -3,59 +3,37 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
-  inputs.hasktorch.url = github:hasktorch/hasktorch;
-  inputs.hasktorch.flake = false;
-
-  inputs.tokenizers.url = github:hasktorch/tokenizers/9d25f0ba303193e97f8b22b9c93cbc84725886c3;
-  inputs.tokenizers.flake = false;
-
-  inputs.typelevel-rewrite-rules.url = github:hasktorch/typelevel-rewrite-rules/4176e10d4de2d1310506c0fcf6a74956d81d59b6;
-  inputs.typelevel-rewrite-rules.flake = false;
-
-  inputs.type-errors-pretty.url = github:hasktorch/type-errors-pretty/32d7abec6a21c42a5f960d7f4133d604e8be79ec;
-  inputs.type-errors-pretty.flake = false;
-
-  inputs.inline-c.url = github:fpco/inline-c/2d0fe9b2f0aa0e1aefc7bfed95a501e59486afb0;
-  inputs.inline-c.flake = false;
+  inputs.hasktorch-nix.url = "github:asgard-labs/hasktorch-nix";
 
   outputs = inputs:
 
     let
+
+      inherit (inputs.nixpkgs) lib;
+
       system = "x86_64-linux";
 
-      overlay = self: super: {
-        libtorch = self.callPackage "${inputs.hasktorch}/nix/libtorch.nix" {
-          cudaSupport = false;
-          device = "cpu";
-        };
-        haskell = let
-            packageOverrides = hself: hsuper: {
-              tokenizers = hself.callCabal2nix "tokenizers" "${inputs.tokenizers}/bindings/haskell/tokenizers-haskell" {};
-              typelevel-rewrite-rules = hself.callCabal2nix "typelevel-rewrite-rules" inputs.typelevel-rewrite-rules {};
-              type-errors-pretty = hself.callCabal2nix "type-errors-pretty" inputs.type-errors-pretty {};
-              inline-c = hself.callCabal2nix "inline-c" "${inputs.inline-c}/inline-c" {};
-              inline-c-cpp = hself.callCabal2nix "inline-c-cpp" "${inputs.inline-c}/inline-c-cpp" {};
+      ghc-name = "ghc924";
 
-              codegen = hself.callCabal2nix "codegen" "${inputs.hasktorch}/codegen" {};
-              libtorch-ffi = self.haskell.lib.compose.appendConfigureFlag "--extra-include-dirs=${self.libtorch.dev}/include/torch/csrc/api/include"
-                (hself.callCabal2nix "libtorch-ffi" "${inputs.hasktorch}/libtorch-ffi" {
-                  torch = self.libtorch;
-                  c10 = self.libtorch;
-                  torch_cpu = self.libtorch;
-                });
-              libtorch-ffi-helper = hself.callCabal2nix "libtorch-ffi-helper" "${inputs.hasktorch}/libtorch-ffi-helper" {};
-              hasktorch = hself.callCabal2nix "hasktorch" "${inputs.hasktorch}/hasktorch" {};
-              examples = hself.callCabal2nix "examples" "${inputs.hasktorch}/examples" {};
-              experimental = hself.callCabal2nix "experimental" "${inputs.hasktorch}/experimental" {};
+      hasktorch-device = "cuda-11"; # There are three variants "cpu" "cuda-10" "cuda-11"
 
-              hasktorch-skeleton = hself.callCabal2nix "hastorch-skeleton" ./hasktorch-skeleton {};
-            };
-          in super.haskell // { inherit packageOverrides; };
-        haskellPackages = pkgs.haskell.packages.ghc924;
-      };
+      overlay = lib.composeManyExtensions [
+        inputs.hasktorch-nix.overlay
+        (self: super: { haskellPackages = self.haskell.packages.${ghc-name}; })
+        (self: super: {
+          haskell = super.haskell // {
+            packageOverrides =
+              lib.composeExtensions
+                super.haskell.packageOverrides
+                (self.haskell.lib.packageSourceOverrides {
+                  hasktorch-skeleton = ./hasktorch-skeleton; });};})
+      ];
 
       pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
 
+      hlib = pkgs.haskell.lib;
+
+      haskPkgs = __mapAttrs (_: ps: ps.haskellPackages) pkgs.hasktorchPkgs;
 
     in
 
@@ -63,26 +41,34 @@
 
         inherit pkgs overlay;
 
-        packages.${system} = with pkgs; {
-          default = haskellPackages.hasktorch-skeleton;
-          hasktorch = haskellPackages.hasktorch;
-          hasktorch-skeleton = haskellPackages.hasktorch-skeleton;
+        packages.${system} = {
+          default = haskPkgs.${hasktorch-device}.hasktorch-skeleton;
+          hasktorch = haskPkgs.${hasktorch-device}.hasktorch;
+          hasktorch-skeleton = __mapAttrs (_: hpkgs: hpkgs.hasktorch-skeleton) haskPkgs;
         };
 
-        devShell.${system} = with pkgs; haskellPackages.shellFor {
-          packages = p: with p; [
-            hasktorch-skeleton
-          ];
-          buildInputs =
-            (with haskellPackages;
-            [ haskell-language-server
-              threadscope
-            ]) ++
-            [
-              ghcid.bin
-              cabal-install
-            ];
-        };
+        devShell.${system} =
+          let
+            hpkgs = haskPkgs.${hasktorch-device};
+          in
+            hpkgs.shellFor {
+
+              packages = p: with p; [
+                hasktorch-skeleton
+              ];
+
+              buildInputs =
+                (with hpkgs;
+                  [ haskell-language-server
+                    threadscope
+                  ]) ++
+                (with pkgs;
+                  [
+                    ghcid.bin
+                    cabal-install
+                  ]);
+            };
+
       };
 
 }
